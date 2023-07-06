6a1355613f28c0e2020a32791e0d736ea1ae922e
Bohns <58178426+alexisbohns@users.noreply.github.com>
2023-07-06 15:36:27 +0200

    Config/8 config seo infos (#9)
    * change favicon
    
    * install and config seo plugin
    
    * config seo of index page
    
    * change site name

--- gridsome.config.js
+++ gridsome.config.js
@@ -2,7 +2,7 @@
 // Learn more: https://gridsome.org/docs/config
 
 module.exports = {
-  siteName: 'Gridsome',
+  siteName: 'Bohns',
   plugins: [
     {
       use: 'gridsome-plugin-pug',
@@ -10,6 +10,9 @@ module.exports = {
         pug: { /* Options for `pug-plain-loader` */ },
         pugLoader: { /* Options for `pug-loader` */ }
       }
+    },
+    {
+      use: 'gridsome-plugin-seo'
     }
   ]
 };
