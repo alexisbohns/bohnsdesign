d1bb8b6cb1c848c2a2ccb1cbe8ec9d0a516ded30
Alexis Bohn <alexis@enerfip.fr>
2023-07-06 15:33:51 +0200

    config seo of index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,23 @@
 <script>
 export default {
-  metaInfo: {
-    title: 'Bohns Design'
+  metaInfo () {
+    return this.$seo({
+      title: 'Alexis Bohn, designer', // Uses the titleTemplate in Gridsome config
+      description: "Head of Design @Enerfip (Finance) since 2018 and consultant for 10 years, I manage and design products, brands, interfaces and communication. I'm also a front-end Vue.js integrator. I own a Master's degree in Experience Design & Art Direction, a Master's degree in Management, and a Bachelor's degree in Sociology. I have experience working with large clients such as EDF and Crédit Agricole Group, as well as with entrepreneurs and non-profit organizations.",
+      keywords: 'alexis bohn, alexis, bohn, bohns, design, ux design, ui design, product design, product management',
+      openGraph: {
+        title: 'Alexis Bohn, designer',
+        description: "Head of Design @Enerfip (Finance) since 2018 and consultant for 10 years, I manage and design products, brands, interfaces and communication. I'm also a front-end Vue.js integrator. I own a Master's degree in Experience Design & Art Direction, a Master's degree in Management, and a Bachelor's degree in Sociology. I have experience working with large clients such as EDF and Crédit Agricole Group, as well as with entrepreneurs and non-profit organizations.",
+        type: 'website'
+      },
+      twitter: {
+        title: 'Alexis Bohn, designer',
+        description: "Head of Design @Enerfip (Finance) since 2018 and consultant for 10 years, I manage and design products, brands, interfaces and communication. I'm also a front-end Vue.js integrator. I own a Master's degree in Experience Design & Art Direction, a Master's degree in Management, and a Bachelor's degree in Sociology. I have experience working with large clients such as EDF and Crédit Agricole Group, as well as with entrepreneurs and non-profit organizations.",
+        type: 'summary'
+      },
+      link: [],   // any links
+      script: []  // any scripts
+    })
   }
 }
 </script>
